29d5295e6b69241fa9786c216b394b50e67a3f24
Ricardo Langner <ricardo.langner@gmail.com>
2019-06-25 21:24:41 +0200

    Updated publish script (satisfy ctan zip rules).

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import zipfile
+import subprocess
 
 # code for zipping from https://stackoverflow.com/questions/14568647/create-zip-in-python
 def zip(src, dst):
@@ -16,22 +17,25 @@ def zip(src, dst):
 
 def publish():
 	dist_folder = './dist'
+	folder_plain = dist_folder+'/plain/cleanthesis'
+	folder_withexample = dist_folder+'/withexample/cleanthesis'
+
 	# remove dist folder and recreate it
 	shutil.rmtree(dist_folder, ignore_errors=True)
 	
 	os.makedirs(dist_folder)
 	print 'Create folder '+dist_folder
 
-	# also publish example thesis
+	# publish example thesis files
 	print 'copy files of example thesis'
-	shutil.copytree(src='./example-thesis', dst=dist_folder+'/bin-we',
+	shutil.copytree(src='./example-thesis', dst=folder_withexample,
 		ignore=shutil.ignore_patterns('*.aux', '*.bbl', '*.bcf', '*.blg', '*.fdb_latexmk', '*.fls', '*.lof', '*.log', '*.lol', '*.lot', '*.out', '*.xml', '*.gz', '*.toc', '.sty'))
 
 	# create doc folder
-	os.makedirs(dist_folder+'/bin/doc')
-	print 'Create folder '+dist_folder+'/bin/doc'
-	os.makedirs(dist_folder+'/bin-we/doc')
-	print 'Create folder '+dist_folder+'/bin-we/doc'
+	os.makedirs(folder_plain+'/doc')
+	print 'Create folder '+folder_plain+'/doc'
+	os.makedirs(folder_withexample+'/doc')
+	print 'Create folder '+folder_withexample+'/doc'
 
 	files = [
 		('./CHANGELOG.md', 'CHANGELOG.md'),
@@ -44,20 +48,24 @@ def publish():
 	]
 
 	for file in files:
-		print 'copy {} to {}'.format(file[0], dist_folder+'/bin/'+file[1])
-		shutil.copyfile(file[0], dist_folder+'/bin/'+file[1])
-		print 'copy {} to {}'.format(file[0], dist_folder+'/bin-we/'+file[1])
-		shutil.copyfile(file[0], dist_folder+'/bin-we/'+file[1])
+		print 'copy {} to {}'.format(file[0], folder_plain+'/'+file[1])
+		shutil.copyfile(file[0], folder_plain+'/'+file[1])
+		print 'copy {} to {}'.format(file[0], folder_withexample+'/'+file[1])
+		shutil.copyfile(file[0], folder_withexample+'/'+file[1])
 
 	# get the current version
 	f = open('VERSION')
 	current_version = f.readline()
 	f.close()
 
+	# fix file permissions
+	subprocess.call(['chmod', '-R', '775', folder_plain])
+	subprocess.call(['chmod', '-R', '775', folder_withexample])
+
 	# zip the release of the style (without example thesis)
-	zip(dist_folder+'/bin', dist_folder+'/cleanthesis_v'+current_version)
+	zip(folder_plain+'/..', dist_folder+'/cleanthesis_v'+current_version)
 	# zip the release of the style including example thesis
-	zip(dist_folder+'/bin-we', dist_folder+'/cleanthesis-incl-example_v'+current_version)
+	zip(folder_withexample+'/..', dist_folder+'/cleanthesis-incl-example_v'+current_version)
 
 if __name__ == "__main__":
 	publish()
